e2199bbf55d572f7330e6bf3b7c5469ff550a801
vishanth <vishanthkannan777@gmail.com>
2025-12-07 09:54:20 +0530

    feat: add base application layout for face login UI

--- face-login-frontend/src/App.jsx
+++ face-login-frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={styles.container}>
+      <h1 style={styles.title}>Face Login System</h1>
+
+      <div style={styles.box}>
+        <p style={styles.info}>Webcam preview will appear here soon...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button style={styles.button}>Login with Face ID</button>
+    </div>
+  );
 }
 
-export default App
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column", // Stacks items vertically
+    alignItems: "center",     // Centers items horizontally (Left/Right)
+    justifyContent: "center", // Centers items vertically (Top/Bottom)
+    minHeight: "100vh",       // Makes the container take up full screen height
+    padding: "30px",
+    fontFamily: "Arial",
+    backgroundColor: "#f9f8d8ff", // Added a light background to see the centering better
+  },
+  title: {
+    fontSize: "32px",
+    marginBottom: "20px",
+  },
+  box: {
+    width: "350px",
+    height: "260px",
+    margin: "0 auto",
+    border: "2px dashed #605e60ff",
+    borderRadius: "10px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  info: {
+    color: "#494949ff",
+    fontSize: "16px",
+  },
+  button: {
+    marginTop: "25px",
+    padding: "12px 25px",
+    fontSize: "18px",
+    cursor: "pointer",
+    borderRadius: "100px",
+    border: "none",
+    backgroundColor: "#0ffa1fa0",
+    color: "white",
+  },
+};
+
+export default App;
